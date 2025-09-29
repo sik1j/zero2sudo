31e8b7dfe440202d3a7fc22dd01e0b8ca2694191
sik1j <karkisikij2@gmail.com>
2025-09-29 15:40:41 -0700

    refactor into lib for easier integration testing

---
+++ tests/health_test.rs
@@ -0,0 +1,20 @@
+#[tokio::test]
+async fn health_check_works() {
+    spawn_app();
+    let client = reqwest::Client::new();
+
+    let response = client
+        .get("http://127.0.0.1:8000/health_check")
+        .send()
+        .await
+        .expect("Failed to execute request.");
+
+    assert!(response.status().is_success());
+    assert_eq!(Some(0), response.content_length());
+}
+
+// Launch our application in the background ~somehow~
+fn spawn_app() {
+    let server = zero2prod::run().expect("Failed to spawn our app.");
+    let _ = tokio::spawn(server);
+}
